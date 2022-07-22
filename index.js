0dffc91917b173eda9c9e844ed8928b513530f5c
Vladimir Makarov <vladimir.php@gmail.com>
2022-07-22 13:44:41 +0300

    Code stabilization

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path')
 const parseUrl = require('body-parser')
-const { KnishIOClient } = require( "@wishknish/knishio-client-js/src" );
 const PORT = process.env.PORT || 5000
 const encodeUrl = parseUrl.urlencoded({ extended: false })
+const { KnishIOClient } = require( '@wishknish/knishio-client-js/dist/client.umd.js' );
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .set('views', path.join(__dirname, '../views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/', encodeUrl, (req, res) => {
