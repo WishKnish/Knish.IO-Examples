4f2f2cf364072ccea9694fd37d1f3222c6c9e992
Eugene Teplitsky <eugene@wishknish.com>
2022-07-22 17:25:00 -0400

    Completing initial capabilities and cleaning up files

--- index.js
+++ index.js
@@ -1,59 +1,71 @@
-const express = require('express')
-const path = require('path')
-const parseUrl = require('body-parser')
+const express = require( 'express' )
+const path = require( 'path' )
+const parseUrl = require( 'body-parser' )
 const PORT = process.env.PORT || 5000
-const encodeUrl = parseUrl.urlencoded({ extended: false })
-const { KnishIOClient, generateSecret } = require( '@wishknish/knishio-client-js/dist/client.umd.js' );
-const secret = generateSecret();
+const encodeUrl = parseUrl.urlencoded( { extended: false } )
+const { KnishIOClient } = require( '@wishknish/knishio-client-js/dist/client.umd.js' );
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, '../views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .post('/', encodeUrl, async (req, res) => {
-    console.log('Form request:', req.body)
+  .use( '/public', express.static( __dirname + '/public' ) )
+  .set( 'views', path.join( __dirname, 'views' ) )
+  .set( 'view engine', 'ejs' )
+  .get( '/', ( req, res ) => res.render( 'pages/index' ) )
+  .post( '/', encodeUrl, async ( req, res ) => {
+    console.log( 'Form request:', req.body )
 
-
+    // Instantiate the Knish.IO client
     const client = new KnishIOClient( {
       uri: req.body.uri,
       cellSlug: req.body.cell,
       serverSdkVersion: req.body.legacy === 'true' ? 2 : 3
     } );
-    //  console.log(client);
 
+    if ( req.body.legacy !== 'true' ) {
 
-    // Request an auth token
-    let response = await client.requestAuthToken( {
-        secret,
-        cellSlug: req.cell
-    } );
-    //  console.log(response);
+      // Request an auth token
+      await client.requestAuthToken( {
+        seed: req.body.seed,
+      } );
 
+    }
 
-    let metaType = 'testMetaType';
-    let metaId = 'testMetaId';
+    let response;
+    try {
+      switch ( req.body.operation ) {
 
-    // Create a meta instance
-    response = await client.createMeta( {
-    metaType,
-    metaId,
-      meta: {
-        testKey1: 'testValue1',
-        testKey2: 'testValue2'
-      },
-    } );
-    //  console.log(response);
+        case 'read':
+          // Query created meta
+          response = await client.queryMeta( {
+            metaType: req.body.metatype,
+            metaId: req.body.metaid
+          } )
+          break;
 
-    // Query created meta
-    response = await client.queryMeta( {
-        metaType,
-        metaId
-    } )
-    console.log(response);
-    console.log(response[ 'instances' ][ 0 ].metas);
+        case 'write':
+          // Create new meta
+          const meta = {};
+          meta[ req.body.key ] = req.body.value;
+          response = await client.createMeta( {
+            metaType: req.body.metatype,
+            metaId: req.body.metaid,
+            meta
+          } );
 
-
-    res.sendStatus(200)
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+          if ( response.success() ) {
+            response = {
+              molecule: response.molecule()
+            };
+          } else {
+            response = {
+              error: response.reason()
+            }
+          }
+          break;
+      }
+    } catch ( e ) {
+      response.error = e;
+    }
+    response.operation = req.body.operation;
+    res.send( response );
+  } )
+  .listen( PORT, () => console.log( `Listening on ${ PORT }` ) )
---
+++ public/knishio.js
@@ -0,0 +1,177 @@
+$( 'document ' ).ready( function () {
+
+  // Hiding "write" fields by default
+  $( '#meta' ).addClass( 'collapse' );
+
+  // Showing "write" fields if the option is selection on load
+  if ( $( "input[name='operation'][value='write']" ).prop( 'checked' ) ) {
+    $( '#meta' ).collapse( 'show' );
+  }
+
+  // Ensuring that selecting "write" exposes relevant fields
+  $( "input[name='operation']" ).change( function () {
+    if ( $( "input[name='operation'][value='write']" ).prop( 'checked' ) ) {
+      $( '#meta' ).collapse( 'show' );
+    } else {
+      $( '#meta' ).collapse( 'hide' );
+    }
+  } );
+
+} );
+
+/**
+ * Handles form submit event
+ */
+function submit () {
+
+  // Resetting output UX
+  $( '#response' ).collapse( 'hide' );
+  $( '#error' ).collapse( 'hide' );
+  $( '#response .contents' ).html( '' );
+  $( '#error .contents' ).html( '' );
+
+  // Collecting form data
+  const datastring = $( '#form' ).serialize();
+
+  // POSTing AJAX request
+  $.ajax( {
+    type: 'POST',
+    url: '/',
+    data: datastring,
+    dataType: 'json',
+    success: function ( data ) {
+
+      // Showing response block
+      $( '#response' ).collapse( 'show' );
+
+      // Populating response block with content
+      switch ( data.operation ) {
+        case 'read':
+          formatReadResponse( data );
+          break;
+        case 'write':
+          formatWriteResponse( data );
+          break;
+      }
+
+    },
+    error: function () {
+
+      // Showing error block
+      $( '.error' ).collapse( 'show' );
+
+      // Populating error block with content
+      $( '#error > .contents' ).append( 'There was an error submitting your request!' );
+
+    },
+  } );
+}
+
+/**
+ * Formats the meta asset query response into a data table
+ * @param data
+ */
+function formatReadResponse ( data ) {
+
+  let contents = '';
+
+  // Render table header
+  data.instances.forEach( instance => {
+    contents += `
+      <table id="instancesTable${ instance.metaId }" class="table">
+        <thead>
+          <tr>
+            <th colspan="4" class="text-center">
+              ${ instance.metaType }: ${ instance.metaId }
+            </th>
+          </tr>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Key</th>
+            <th scope="col">Value</th>
+            <th scope="col">Timestamp</th>
+          </tr>
+        </thead>
+        <tbody>
+    `;
+
+    // Render table body
+    for ( let metaCount = 0; metaCount < instance.metas.length; metaCount++ ) {
+      contents += `
+          <tr>
+            <th scope="row">${ metaCount + 1 }</th>
+            <td>${ instance.metas[ metaCount ].key }</td>
+            <td>${ instance.metas[ metaCount ].value }</td>
+            <td>${ formatDate( instance.metas[ metaCount ].createdAt ) }</td>
+          </tr>
+      `;
+    }
+
+    // Render table footer
+    contents += `
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="4" class="text-right">
+              Last updated at ${ formatDate( instance.createdAt ) }
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    `;
+
+    // Appending table content to the block
+    $( '#response > .contents' ).append( contents );
+
+    // Activating data table overlay
+    $( `#instancesTable${ instance.metaId }` ).DataTable();
+
+  } );
+
+}
+
+/**
+ * Formats the meta asset write response
+ * @param data
+ */
+function formatWriteResponse ( data ) {
+
+  let contents = '';
+
+  if( data.molecule ) {
+    contents += `
+      Molecule ${ data.molecule.molecularHash }, created at ${ formatDate( data.molecule.createdAt ) }, was ${ data.molecule.status }.
+    `;
+  }
+  else {
+    contents += `
+      Unable to retrieve molecule data. Please review server output.
+    `;
+  }
+
+  $( '#response > .contents' ).append( contents );
+}
+
+/**
+ * Formats a Knish.IO timestamp into a human-readable, sortable date
+ * @param timestamp
+ * @returns {string}
+ */
+function formatDate ( timestamp ) {
+  const date = new Date( Number( timestamp ) );
+  let month = date.getMonth();
+  month = month + 1; //javascript date goes from 0 to 11
+  if ( month < 10 ) {
+    month = '0' + month;
+  } //adding the prefix
+
+  const year = date.getFullYear();
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  return year + "/" + month + "/" + day + " " + hour + ":" + minutes + ":" + seconds;
+
+}
